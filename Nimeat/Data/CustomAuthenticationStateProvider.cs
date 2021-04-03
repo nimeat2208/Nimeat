016be2a680249b11a4a4fc40c8b2656c96817ff0
KTom1108 <ktm911108@gmail.com>
2021-04-03 14:09:39 +0900

    초기 커밋.

---
+++ Nimeat/Data/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,70 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Nimeat.Data
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private ISessionStorageService _sessionStorageService;
+        public CustomAuthenticationStateProvider(ISessionStorageService sessionStorageService)
+        {
+            _sessionStorageService = sessionStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var emailAddress = await _sessionStorageService.GetItemAsync<string>("emailAddress");
+            ClaimsIdentity identity;
+
+            if (emailAddress != null)
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, emailAddress)
+                }, "apiauth_type");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }
+
+            var user = new ClaimsPrincipal(identity);
+
+            return await Task.FromResult(new AuthenticationState(user));
+        }
+        public void MarkUserAsAuthenticated(string emailAddress)
+        {
+            ClaimsIdentity identity;
+            if (emailAddress == "esjang1115@gmail.com")
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, emailAddress)
+                }, "apiauth_type");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            _sessionStorageService.RemoveItemAsync("emailAddress");
+
+            var identity = new ClaimsIdentity();
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+    }
+
+
+}
