f0a8e9c11b7d85fcc3675746a83b61d88f8c3f39
tmkim <tmkim@VM0197>
2021-04-03 14:51:26 +0900

    test

--- Nimeat/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ Nimeat/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,64 +13,99 @@ namespace Nimeat.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 1 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 2 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 3 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 4 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 5 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 6 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 7 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 8 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Nimeat;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Taemin.K\source\repos\Nimeat\Nimeat\_Imports.razor"
+#line 9 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
 using Nimeat.Shared;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 10 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
+using Microsoft.Extensions.Configuration;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
+using System.Threading;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
+using Microsoft.Extensions.Localization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
+using Nimeat.Data;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 14 "C:\Users\tmkim\Source\Repos\nimeat2208\Nimeat\Nimeat\_Imports.razor"
+using System.Text.RegularExpressions;
 
 #line default
 #line hidden
@@ -83,6 +118,8 @@ using Nimeat.Shared;
         {
         }
         #pragma warning restore 1998
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IStringLocalizer<App> Localizer { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IJSRuntime JSRuntime { get; set; }
     }
 }
 #pragma warning restore 1591
